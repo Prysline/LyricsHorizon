0f016ec99eeda5a9650434f39190a8bc3014c26c
prysline <mayhu1993@gmail.com>
2024-12-24 15:15:42 +0800

    chore: fix astro config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site:
+    process.env.NODE_ENV === 'production' ? 'https://prysline.github.io' : 'http://localhost:4321',
+  base: process.env.NODE_ENV === 'production' ? '/LyricsHorizon' : '',
   integrations: [tailwind()],
   compilerOptions: {
     strict: true,
     strictNullChecks: true,
   },
-})
+});
